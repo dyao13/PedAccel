4b8aafac641be7ece53e5ec4618dc3fc199d6816
Daniel Yao <Daniel Yao>
2025-02-10 15:35:38 -0500

    times

--- ECG/pain.py
+++ ECG/pain.py
@@ -8,6 +8,40 @@ from scipy import stats
 
 import preprocessing
 
+def match_times(df1, df2):
+    """
+    Concatenates two dataframes based on matching times
+
+    Args:
+        df1 (pd.DataFrame): Dataframe with 'Time'
+        df2 (pd.DataFrame): Dataframe with 'start_time' and 'end_time'
+
+    Returns:
+        pd.DataFrame: Concatenated dataframe with matching times
+    """
+    index1 = 0
+    index2 = 0
+
+    mask1 = np.zeros(len(df1), dtype=bool)
+    mask2 = np.zeros(len(df2), dtype=bool)
+
+    while index1 < len(df1) and index2 < len(df2):
+        if df1['Time'][index1] < df2['start_time'][index2]:
+            index1 += 1
+        elif df1['Time'][index1] > df2['end_time'][index2]:
+            index2 += 1
+        else:
+            mask1[index1] = True
+            mask2[index2] = True
+
+            index1 += 1
+            index2 += 1
+
+    df1 = df1[mask1].reset_index(drop=True)
+    df2 = df2[mask2].reset_index(drop=True)
+
+    return pd.concat([df1, df2], axis=1)
+
 def find_agitation(df):
     pattern = r'agitated|upset|fussy'
     mask = (df['Notes_TM'].str.contains(pattern, case=False, na=False))
@@ -58,14 +92,19 @@ def main():
         retro_df.insert(0, 'Time', preprocessing.format_times(retro_df['Time_uniform']))
         retro_df.drop(columns=['Time_uniform', 'Datetime'], inplace=True)
 
-        raw_sickbay['start_time'] = preprocessing.format_times(raw_sickbay['start_time'])
-        raw_sickbay['end_time'] = preprocessing.format_times(raw_sickbay['end_time'])
+        # raw_sickbay['start_time'] = preprocessing.format_times(raw_sickbay['start_time'])
+        # raw_sickbay['end_time'] = preprocessing.format_times(raw_sickbay['end_time'])
 
         raw_ecg['start_time'] = preprocessing.format_times(raw_ecg['start_time'])
         raw_ecg['end_time'] = preprocessing.format_times(raw_ecg['end_time'])
 
-        sickbay_df = pd.DataFrame(raw_sickbay)
+        # sickbay_df = pd.DataFrame(raw_sickbay)
         ecg_df = pd.DataFrame(raw_ecg)
+        ecg_df.drop(columns=['sbs_score'], inplace=True)
 
+        retro_df = retro_df[['Time', 'TM SBS']]
+        match_df = match_times(retro_df, ecg_df)
+        print(match_df)
+        
 if __name__ == "__main__":
     main()
