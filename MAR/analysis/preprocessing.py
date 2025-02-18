91c7e3139a0e66a8346c35fbb2e81a86555d37e8
Daniel Yao <Daniel Yao>
2025-02-17 20:29:12 -0500

    oop

--- MAR/analysis/preprocessing.py
+++ MAR/analysis/preprocessing.py
@@ -1,87 +1,120 @@
 import os
 from scipy.io import loadmat
-from tqdm import tqdm
 import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
 
-def load_mar_data(data_dir, pat_num):
+class Preprocessing:
     """
-    Loads MAR data from a .csv file and returns it as a pandas DataFrame.
-
-    Args:
-        data_dir (str): Path to the directory containing the .mat file.
-        pat_num (int): Patient number.
-
-    Returns:
-        pd.DataFrame: DataFrame containing MAR data.
+    Preprocessing class for loading and processing MAR data.
     """
-    if not os.path.exists(data_dir):
-        raise FileNotFoundError(f"The directory {data_dir} does not exist.")
-    
-    file_path = os.path.join(data_dir, f'Patient{pat_num}_SickBayMARData.csv')
 
-    if os.path.exists(file_path):
-        df = pd.read_csv(file_path)
-    else:
-        raw_data = load_mat_file(os.path.join(data_dir, f'Patient{pat_num}_SickBayMARData.mat'))
-        df = dict_to_df(raw_data, file_path)
+    @staticmethod
+    def load_mar_data(data_dir, pat_num):
+        """
+        Loads MAR data from a .csv file and returns it as a pandas DataFrame.
+
+        Parameters:
+            data_dir (str): Path to the directory containing the .mat file.
+            pat_num (int): Patient number. 
+
+        Returns:
+            pd.DataFrame: DataFrame containing MAR data.
+        """    
+        file_path = os.path.join(data_dir, f'Patient{pat_num}_SickBayMARData.csv')
+
+        if os.path.exists(file_path):
+            df = pd.read_csv(file_path)
+        else:
+            raw_data = Preprocessing.load_mat_file(os.path.join(data_dir, f'Patient{pat_num}_SickBayMARData.mat'))
+            df = Preprocessing.dict_to_df(raw_data, file_path)
+
         df = df.drop(columns=['description'])
+        df.rename(columns={'mar_time': 'time'}, inplace=True)
+        df = df[['time', 'dose', 'mar_action', 'med_name']]
 
-    return df
+        return df
 
-def load_mat_file(file_path):
-    """
-    Loads .mat file and returns a dictionary.
+    @staticmethod
+    def load_mat_file(file_path):
+        """
+        Loads .mat file and returns a dictionary.
 
-    Args:
-        file_path (str): Path to .mat file.
+        Args:
+            file_path (str): Path to .mat file.
 
-    Raises:
-        FileNotFoundError: If the file does not exist.    
+        Parameters:
+            FileNotFoundError: If the file does not exist.    
 
-    Returns:
-        dict: Dictionary containing data from .mat file.
-    """
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
-    
-    raw_data = loadmat(file_path)
+        Returns:
+            dict: Dictionary containing data from .mat file.
+        """
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"The file {file_path} does not exist.")
+        
+        raw_data = loadmat(file_path)
 
-    for key in list(raw_data.keys()):
-        if key.startswith("__"):
-            del raw_data[key]
+        for key in list(raw_data.keys()):
+            if key.startswith("__"):
+                del raw_data[key]
 
-    return raw_data
+        return raw_data
 
-def dict_to_df(raw_data, save_path = None):
-    """
-    Converts dictionary to pandas DataFrame and (optionally) saves it as a .csv file.
+    @staticmethod
+    def dict_to_df(raw_data, save_path = None):
+        """
+        Converts dictionary to pandas DataFrame and (optionally) saves it as a .csv file.
 
-    Args:
-        raw_data (dict): Dictionary containing data.
-        save_path (str, optional): Path to save .csv file. Defaults to None.
+        Parameters:
+            raw_data (dict): Dictionary containing data.
+            save_path (str, optional): Path to save .csv file. Defaults to None.
 
-    Returns:
-        pd.DataFrame: DataFrame containing data.
-    """
-    for key in raw_data.keys():
-        try:
-            raw_data[key] = raw_data[key].squeeze()
-        except:
-            pass
+        Returns:
+            pd.DataFrame: DataFrame containing data.
+        """
+        for key in raw_data.keys():
+            try:
+                raw_data[key] = raw_data[key].squeeze()
+            except:
+                pass
 
-    df = pd.DataFrame(raw_data)
+        df = pd.DataFrame(raw_data)
 
-    if save_path is not None:
-        df.to_csv(save_path, index=False)
-    
-    return df
+        if save_path is not None:
+            df.to_csv(save_path, index=False)
+        
+        return df
+
+    @staticmethod
+    def match_times(df1, df2):
+        """
+        Matches times between two DataFrames.
+        
+        Parameters:
+            df1 (pd.DataFrame): First DataFrame.
+            df2 (pd.DataFrame): Second DataFrame.
+
+        Returns:
+            pd.DataFrame: DataFrame containing matched times.
+        """
+        if df1.empty or df2.empty:
+            return pd.DataFrame()
+
+    def __match_times_time_time(df1, df2):
+        pass
+
+    def __match_times_time_startend(df1, df2):
+        pass
+
+    def __match_times_startend_startend(df1, df2):
+        pass
 
 def main():
     data_dir = os.path.join(os.path.dirname(__file__), "../data")
     pat_nums = [2, 3, 4, 5, 6, 8, 9, 11, 12, 13, 15]
 
     for pat_num in tqdm(pat_nums):
-        df = load_mar_data(data_dir, pat_num)
+        df = Preprocessing.load_mar_data(data_dir, pat_num)
 
         print(df.keys())
         print(df.shape)
